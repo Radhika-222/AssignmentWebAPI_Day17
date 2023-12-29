d4223e741bd86792609ac0f178253d8211a24669
Radhika-222 <wandareradhika@gmail.com>
2023-12-29 20:30:15 +0530

    Assignment WebAPI

---
+++ AssignmentDay17_WebApi/AssignmentDay17_WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssignmentDay17_WebApi.MvcApplication" Language="C#" %>
---
+++ AssignmentDay17_WebApi/AssignmentDay17_WebApi/Models/Teams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentDay17_WebApi.Models
+{
+    [Table("Teams")]
+    public class Teams
+    {
+        [Key]
+        public int TId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TEmail { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string SLogan { get; set; }
+    }
+}
